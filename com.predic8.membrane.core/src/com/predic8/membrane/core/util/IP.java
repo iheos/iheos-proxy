ec83d9837004c0cab6aa2b56bb6ceb8b118801b8
Sunil Bhaskarla <skbhaskarla@gmail.com>
2014-03-26 16:14:58 -0400

    1) Update exchange host details: use real target and proxy port identifiers and 2) Fix Ipv4 and Ipv6 address resolution.

--- com.predic8.membrane.core/src/com/predic8/membrane/core/util/IP.java
+++ com.predic8.membrane.core/src/com/predic8/membrane/core/util/IP.java
@@ -6,7 +6,11 @@ import java.net.UnknownHostException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-
+/**
+ * 
+ * @author Sunil.Bhaskarla
+ *
+ */
 public class IP {
 	private static Log log = LogFactory.getLog(IP.class.getName());
 	
@@ -14,11 +18,22 @@ public class IP {
 			  try {
 			   String ipaddr = InetAddress.getByName(hostname).getHostAddress() ;
 			   return ipaddr ;
+				  
+			
 			   }
 			  catch(UnknownHostException uhe) {
 				  log.error("IP: Unknown host:"+hostname);
 			  }
 			  return null;
-		 }		
+		 }
+		 
+		 public InetAddress[] getAllIPs(String hostname) {
+			try {
+				  return InetAddress.getAllByName(hostname);
+			} catch (UnknownHostException uhe) {
+				log.error(uhe.toString());
+			}
+			return null;
+		 }
 }
 
